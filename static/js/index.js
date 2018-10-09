468bc007683d474498d9d209fb850259f29ebf45
Pavel Pauliushchyk <p.pavliushchik@aisnovations.com>
2018-10-09 17:58:45 +0300

    added date validation

--- static/js/index.js
+++ static/js/index.js
@@ -5,3 +5,15 @@ if (backButton) {
     window.history.back();
   });
 }
+
+const inputs = document.querySelectorAll('input[name=birth_date]');
+
+if (inputs) {
+  inputs.forEach(input => {
+    const date = new Date();
+
+    input.setAttribute('max', `${date.getFullYear() - 18}-12-31`);
+
+    input.setAttribute('min', `${date.getFullYear() - 60}-01-01`);
+  });
+}
